291586f10d29590c7afee20500ac27ffbf912f4a
antho <anthony-flores_@hotmail.com>
2025-12-04 16:56:25 -0500

    Agrgando Iframes

---
+++ example/iframes_ventanas.js
@@ -0,0 +1,13 @@
+const {chromium} = require ('playwright');
+
+(async () => {
+    const browser = await chromium.launch({headless: false});
+
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto('https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_iframe')
+
+
+
+
+})();

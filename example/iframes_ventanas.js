b37c68e3d51a473fd8591672923c7b7c2dd00507
antho <anthony-flores_@hotmail.com>
2025-12-15 10:31:50 -0500

    Examples con js

--- example/iframes_ventanas.js
+++ example/iframes_ventanas.js
@@ -1,3 +1,6 @@
+
+//Localizaon de elementos de iframes 
+
 const {chromium} = require ('playwright');
 
 (async () => {
@@ -7,6 +10,13 @@ const {chromium} = require ('playwright');
     const page = await context.newPage();
     await page.goto('https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_iframe')
 
+    const iframe = await page.frame({name: 'main'});
+    console.log("iframe encontrado");
+
+    if(iframe){
+        await iframe.click('text=HTML');
+        console.log("Se hizo click en HTML");
+    }
 
 
 

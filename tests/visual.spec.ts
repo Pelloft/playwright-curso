382168585a4804858326197f2077220dd872d69d
antho <anthony-flores_@hotmail.com>
2025-12-29 15:42:38 -0500

    Refactorizando codigo

--- tests/visual.spec.ts
+++ tests/visual.spec.ts
@@ -1,8 +1,11 @@
 import {test, expect} from '@playwright/test';
 
 test('Test visual con capturas de pantalla', async ({page}) => {
-    await page.goto('https://www.google.com');
-    const screenshot = await page.screenshot();
+    await page.goto('https://www.saucedemo.com/');
+    //const screenshot = await page.screenshot();
 
-    expect(screenshot).toMatchSnapshot('google-homepage.png');
+    await expect(page.locator('.login_logo')).toBeVisible();
+    
+    //reemplazar toMatchSnapshot a toHaveScreenshot
+    await expect(page).toHaveScreenshot('suacedemo-login.png');
 });
